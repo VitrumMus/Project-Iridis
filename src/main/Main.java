4c2ed4e0a2194d980e11e4700fe64f954031d4a9
Vitrum Mus <VitrumMus@gmail.com>
2013-05-29 22:11:46 -0400

    Development: changed double variable types to int where appropriate

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,21 +13,24 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Main extends StateBasedGame {
 
 	private static final String  GAME_TITLE                   = "Project Iridis";
-	private static final double  GAME_SCREEN_WIDTH_IN_PIXELS  = 1280.0;
-	private static final double  GAME_SCREEN_HEIGHT_IN_PIXELS = 720.0;
+	private static final int     GAME_SCREEN_WIDTH_IN_PIXELS  = 1280;
+	private static final int     GAME_SCREEN_HEIGHT_IN_PIXELS = 720;
 	private static final boolean IS_GAME_FULL_SCREEN          = false;
-	private static final double  GAME_TARGET_FRAME_RATE       = 60.0;
+	private static final int     GAME_TARGET_FRAME_RATE       = 60;
 	private static final boolean IS_GAME_WINDOW_RESIZEABLE    = false;
 
 	public static void main(String[] args) throws SlickException {
 		// can throw SlickException
 		AppGameContainer appGameContainer = new AppGameContainer(new Main(GAME_TITLE));
+
+		// set appGameContainer's properties
 		appGameContainer.setTitle(GAME_TITLE);
 		appGameContainer.setMouseGrabbed(false);
 		appGameContainer.setVSync(true);
-		appGameContainer.setDisplayMode((int) GAME_SCREEN_WIDTH_IN_PIXELS, (int) GAME_SCREEN_HEIGHT_IN_PIXELS, IS_GAME_FULL_SCREEN);
-		appGameContainer.setTargetFrameRate((int) GAME_TARGET_FRAME_RATE);
+		appGameContainer.setDisplayMode(GAME_SCREEN_WIDTH_IN_PIXELS, GAME_SCREEN_HEIGHT_IN_PIXELS, IS_GAME_FULL_SCREEN);
+		appGameContainer.setTargetFrameRate(GAME_TARGET_FRAME_RATE);
 		appGameContainer.setResizable(IS_GAME_WINDOW_RESIZEABLE);
+		appGameContainer.start();
 	}
 
 	public Main(String title) {
