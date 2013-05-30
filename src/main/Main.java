f017c205f008bd914c78aa371672532e17ba3e66
Vitrum Mus <VitrumMus@gmail.com>
2013-05-29 22:43:01 -0400

    Development: added a couple of constant fields

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,12 +12,14 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class Main extends StateBasedGame {
 
-	private static final String  GAME_TITLE                   = "Project Iridis";
-	private static final int     GAME_SCREEN_WIDTH_IN_PIXELS  = 1280;
-	private static final int     GAME_SCREEN_HEIGHT_IN_PIXELS = 720;
-	private static final boolean IS_GAME_FULL_SCREEN          = false;
-	private static final int     GAME_TARGET_FRAME_RATE       = 60;
-	private static final boolean IS_GAME_WINDOW_RESIZEABLE    = false;
+	private static final String  GAME_TITLE                     = "Project Iridis";
+	private static final boolean IS_MOUSE_GRABBED_INITIALLY     = false;
+	private static final int     GAME_SCREEN_WIDTH_IN_PIXELS    = 1280;
+	private static final int     GAME_SCREEN_HEIGHT_IN_PIXELS   = 720;
+	private static final boolean IS_GAME_FULL_SCREEN            = false;
+	private static final int     GAME_TARGET_FRAME_RATE         = 60;
+	private static final boolean IS_GAME_WINDOW_RESIZEABLE      = false;
+	private static final boolean IS_VERTICAL_SYNCHRONISATION_ON = true;
 
 	public static void main(String[] args) throws SlickException {
 		// can throw SlickException
@@ -25,8 +27,8 @@ public class Main extends StateBasedGame {
 
 		// set appGameContainer's properties
 		appGameContainer.setTitle(GAME_TITLE);
-		appGameContainer.setMouseGrabbed(false);
-		appGameContainer.setVSync(true);
+		appGameContainer.setMouseGrabbed(IS_MOUSE_GRABBED_INITIALLY);
+		appGameContainer.setVSync(IS_VERTICAL_SYNCHRONISATION_ON);
 		appGameContainer.setDisplayMode(GAME_SCREEN_WIDTH_IN_PIXELS, GAME_SCREEN_HEIGHT_IN_PIXELS, IS_GAME_FULL_SCREEN);
 		appGameContainer.setTargetFrameRate(GAME_TARGET_FRAME_RATE);
 		appGameContainer.setResizable(IS_GAME_WINDOW_RESIZEABLE);
